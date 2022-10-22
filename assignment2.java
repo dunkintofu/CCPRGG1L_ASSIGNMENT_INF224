dff579bda746af055bceb038232c4dbdabe654ef
Marielle Kyla E. Villena <111546442+dunkintofu@users.noreply.github.com>
2022-10-22 12:08:18 +0800

    Create assignment2.java

---
+++ assignment2.java
@@ -0,0 +1,24 @@
+public class Assignment2 {
+    public static void main(String[] args) throws Exception {
+    int count1 = 0;
+    while (count1 !=3){
+    System.out.println("MARIELLE KYLA");
+    count1 ++;
+    }
+    int count2 = 0;
+    do {
+        System.out.println("VILLENA");
+        count2 ++;
+    } while (count2 != 4);
+    for (int count3 = 0; count3  != 5;  count3 ++) {
+    System.out.println("CADIZ");
+    }
+    char [] myFirstName = {'M', 'A', 'R', 'I', 'E', 'L', 'L', 'E', 'K', 'Y', 'L", 'A'};
+    for (int i = 0; i < myFirstName.length; i++){
+    System.out.println(myFirstName[i]);
+    }  
+    for (int i = myFirstName.length - 1; i >= 0; i--) {
+    System.out.println(myFirstName[i]);
+        }
+    }
+}
